2b07dbff4d1500eacea5595b28cd96f5ada1655a
Johnson2009 <johnson4109@gmail.com>
2024-10-22 18:42:01 +0800

    test update

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
  <!-- <router-link to="/main">Main</router-link>
  <router-link to="/login">Login</router-link>
  <router-link to="/icondemo">IconDemo</router-link> -->
- <router-view></router-view>
+ <router-view> </router-view>
 </div>
 </template>
 
